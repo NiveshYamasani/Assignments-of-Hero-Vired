8115c7c7741f443f99b5fd919ea521eb33f544a6
Nivesh Yamasani <89142861+NiveshYamasani@users.noreply.github.com>
2022-12-11 23:50:53 +0530

    Answers for the assignments given on 9-12-2022

--- Assignment2.py
+++ Assignment2.py
@@ -1,7 +1,11 @@
+n = int(input('Enter the number of entries:')) #Number of entries you want to enter
+Car = {}
 x = open('answer2.txt','a') # Opening files in append mode
-brand_name = input('Enter the brand name of the car :')  # taking input of the brand of the car
-color = input('Enter the color of the car :') # taking input of the color of the car
-Car = {brand_name:color} # Entering the brand and the color into a dictionary
+for i in range(n):
+    brand_name = input('Enter the brand name of the car :')  # taking input of the brand of the car
+    color = input('Enter the color of the car :') # taking input of the color of the car
+    Car[brand_name] = color # Entering the brand and the color into a dictionary
 print(Car)
 print(Car,file=x) # entering into files
 x.close() # closing the file
+
